3eeaa0b1f3daf43a0f657e08da3b010b4c9c5537
David Pradell <david.pradell@moralis.io>
2022-11-07 12:06:06 +0100

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -11,36 +11,87 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private ConnectionScriptableObject connectionScriptableObject;
     
+    // GameSparks requests
     private MyWeb3GameBackendOperations.GetNativeBalanceRequest getNativeBalanceRequest;
+    private MyWeb3GameBackendOperations.GetWalletNftsRequest getWalletNftsRequest;
+    
+    #region UNITY_LIFECYCLE
 
     private void Start()
     {
+        // Initializing requests
         getNativeBalanceRequest = new MyWeb3GameBackendOperations.GetNativeBalanceRequest();
+        getWalletNftsRequest = new MyWeb3GameBackendOperations.GetWalletNftsRequest();
     }
-
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            try
-            {
-                Debug.Log("Sending GetNativeBalance request");
-                connectionScriptableObject.Connection.EnqueueGetNativeBalanceRequest(
-                    getNativeBalanceRequest,
-                    HandleGetNativeBalanceResponse,
-                    error => { Debug.Log("Request failed: " + error); },
-                    () => { Debug.Log("Request timed out."); },
-                    TimeSpan.FromMinutes(2));
-            }
-            catch (Exception e)
-            {
-                Debug.LogException(e);
-            }
+            GetNativeBalance();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetWalletNfts();
         }
     }
+
+    #endregion
+
+    
+    #region PUBLIC_METHODS
+    
+    public void GetNativeBalance()
+    {
+        try
+        {
+            Debug.Log("Sending GetNativeBalance request");
+            connectionScriptableObject.Connection.EnqueueGetNativeBalanceRequest(
+                getNativeBalanceRequest,
+                HandleGetNativeBalanceResponse,
+                error => { Debug.Log("Request failed: " + error); },
+                () => { Debug.Log("Request timed out."); },
+                TimeSpan.FromMinutes(2));
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+    
+    public void GetWalletNfts()
+    {
+        try
+        {
+            Debug.Log("Sending GetNativeBalance request");
+            connectionScriptableObject.Connection.EnqueueGetWalletNftsRequest(
+                getWalletNftsRequest,
+                HandleGetWalletNftsRequest,
+                error => { Debug.Log("Request failed: " + error); },
+                () => { Debug.Log("Request timed out."); },
+                TimeSpan.FromMinutes(2));
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+
+    #endregion
+    
+    
+    #region RESPONSE_HANDLERS
 
     private void HandleGetNativeBalanceResponse(Message<MyWeb3GameBackendOperations.GetNativeBalanceResponse> response)
     {
         Debug.Log(response.Payload.result);
     }
+    
+    private void HandleGetWalletNftsRequest(Message<MyWeb3GameBackendOperations.GetWalletNftsResponse> response)
+    {
+        Debug.Log(response.Payload.result);
+    }
+
+    #endregion
 }
